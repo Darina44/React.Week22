f53233cc1f0b780cfba123f89e15b4a2e8f3d593
darina bebenina <darinabebenina@yahoo.com>
2023-06-18 00:56:44 +0500

    begin

---
+++ my-app/src/components/Hero.jsx
@@ -0,0 +1,12 @@
+import './Hero.css';
+
+function Hero() {
+    return (
+        <div className="Hero-card">
+            <img src="../images/batman.jpeg" alt="batman" />
+        </div>
+
+    );
+}
+
+export default Hero;
